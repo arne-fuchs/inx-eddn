cf10609dfe57dc57fb3d01668e4491d1f0e95523
arne-fuchs <arnefuchs@protonmail.com>
2023-09-14 18:55:30 +0200

    Implemented iota-sdk and new protocol

--- src/hornet_adapter.rs
+++ src/hornet_adapter.rs
@@ -1,13 +1,21 @@
 use std::time::Instant;
+use base64::Engine;
+use base64::engine::general_purpose;
 
 use bus::BusReader;
-use iota_wallet::account::AccountHandle;
-use iota_wallet::iota_client::block::Block;
-use iota_wallet::iota_client::Client;
+use iota_sdk::client::Client;
+use iota_sdk::client::constants::SHIMMER_COIN_TYPE;
+use iota_sdk::client::secret::SecretManage;
+use iota_sdk::client::secret::stronghold::StrongholdSecretManager;
+use iota_sdk::crypto::keys::bip44::Bip44;
+use iota_sdk::wallet::Account;
+use serde_json::json;
+use rustc_hex::ToHex;
 
 pub struct Hornet {
     pub node: Client,
-    pub account: AccountHandle,
+    pub account: Account,
+    pub stronghold: StrongholdSecretManager,
     pub bus_reader: BusReader<Vec<u8>>,
 }
 
@@ -22,9 +30,40 @@ impl Hornet {
             .build()
             .unwrap()
             .block_on(async move {
-                let result = thread_node.block()
+
+                let bip44_chain = Bip44::new(SHIMMER_COIN_TYPE)
+                    .with_account(0)
+                    .with_change(false as _)
+                    .with_address_index(0);
+
+                let ed255195_signature = self.stronghold
+                    .sign_ed25519(&thread_data, bip44_chain)
+                    .await.unwrap();
+
+                let mut signature: String = "0x".to_string();
+                let tmp_sig: String = ed255195_signature.signature().to_bytes().to_hex();
+                signature.push_str(tmp_sig.as_str());
+
+                let mut public_key: String = "0x".to_string();
+                let tmp_pk: String = ed255195_signature.public_key().to_bytes().to_hex();
+                public_key.push_str(tmp_pk.as_str());
+
+                let message_data = general_purpose::STANDARD.encode(thread_data);
+
+
+                let message = json!(
+                                    {
+                                        "message": message_data,
+                                        "signature": signature,
+                                        "public_key": public_key
+                                    }
+                                );
+
+                println!("{}",message);
+
+                let result = thread_node.build_block()
                     .with_tag("EDDN".as_bytes().to_vec())
-                    .with_data(thread_data)
+                    .with_data(message.to_string().as_bytes().to_vec())
                     .finish()
                     .await;
 
--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,15 @@ use std::time::Duration;
 
 use bus::Bus;
 use dotenv::dotenv;
-use iota_wallet::ClientOptions ;
-use iota_wallet::account_manager::AccountManager;
-use iota_wallet::iota_client::constants::SHIMMER_COIN_TYPE;
-use iota_wallet::iota_client::generate_mnemonic;
-use iota_wallet::secret::stronghold::StrongholdSecretManager;
+use iota_sdk::client::constants::SHIMMER_COIN_TYPE;
+use iota_sdk::client::generate_mnemonic;
+use iota_sdk::client::secret::{SecretManage, SecretManager};
+use iota_sdk::client::secret::stronghold::StrongholdSecretManager;
+use iota_sdk::crypto::keys::bip44::Bip44;
+use iota_sdk::types::block::address::ToBech32Ext;
+use iota_sdk::Wallet;
+use iota_sdk::wallet::ClientOptions;
+use rustc_hex::ToHex;
 use tokio::time::Instant;
 
 use crate::eddn_adapter::EddnAdapter;
@@ -20,7 +24,7 @@ use crate::hornet_adapter::Hornet;
 mod hornet_adapter;
 mod eddn_adapter;
 
-fn main(){
+fn main() {
     dotenv().expect(".env file not found");
 
     let mut hornet_bus: Bus<Vec<u8>> = Bus::new(1000);
@@ -28,16 +32,20 @@ fn main(){
 
     println!("Loading wallet");
 
+    let workers: usize = std::env::var("NUM_OF_WORKERS").unwrap().parse().unwrap();
 
     let client_options = ClientOptions::new()
         .with_local_pow(true)
-        .with_pow_worker_count(std::env::var("NUM_OF_WORKERS").unwrap().parse().unwrap())
+        .with_pow_worker_count(workers)
         .with_node(std::env::var("NODE_URL").unwrap().as_str()).unwrap();
 
-
+    let secret_manager = StrongholdSecretManager::builder()
+        .password(std::env::var("WALLET_PASSWORD").unwrap().to_string())
+        .build("wallet.stronghold").unwrap();
 
     //create stronghold account
     let wallet_file_result = File::open("wallet.stronghold");
+
 
     let account = match wallet_file_result {
         Ok(file) => {
@@ -48,69 +56,59 @@ fn main(){
                 .build()
                 .unwrap()
                 .block_on(async {
-                    // Create the account managero
-                    let manager = AccountManager::builder()
+                    let stronghold = SecretManager::Stronghold(secret_manager);
+
+                    let wallet = Wallet::builder()
+                        .with_secret_manager(stronghold)
                         .with_client_options(client_options)
                         .with_coin_type(SHIMMER_COIN_TYPE)
                         .finish().await.unwrap();
 
-                    // Set the stronghold password
-                    manager
-                        .set_stronghold_password(std::env::var("WALLET_PASSWORD").unwrap().as_str())
-                        .await.unwrap();
+                    let account = wallet
+                        .get_account("User").await.unwrap();
 
-                    // Get the account we generated with `01_create_wallet`
-                    let account = manager.get_account("User").await.unwrap();
+                    println!("{:?}", account.client());
 
-                    println!("{:?}",account.client());
-
-                    println!("Bech32: {}",account.client().get_bech32_hrp().await.unwrap());
+                    println!("Bech32: {}", account.client().get_bech32_hrp().await.unwrap());
 
                     let mut balance_result = account.sync(None).await;
                     while balance_result.is_err() {
-                        println!("{}",balance_result.err().unwrap());
+                        println!("{}", balance_result.err().unwrap());
                         sleep(Duration::from_secs(2));
                         balance_result = account.sync(None).await;
                     }
 
                     let balance = balance_result.unwrap();
 
-                    println!("[Total: {} : Available: {}]",balance.base_coin.total,balance.base_coin.available);
-                    println!("[NFTS Count: {}]",balance.nfts.len());
-                    println!("[Req. storage deposit (basic): {}]",balance.required_storage_deposit.basic());
+                    println!("[Total: {} : Available: {}]", balance.base_coin().total(), balance.base_coin().available());
+                    println!("[NFTS Count: {}]", balance.nfts().len());
+                    println!("[Req. storage deposit (basic): {}]", balance.required_storage_deposit().basic());
 
                     return account;
                 })
         }
         Err(err) => {
             println!("{}", &err);
-            println!("{}",err);
+            println!("{}", err);
             println!("Stronghold file not found -> creating");
             tokio::runtime::Builder::new_current_thread()
                 .enable_all()
                 .build()
                 .unwrap()
                 .block_on(async {
-                    // Setup Stronghold secret_manager
-                    let mut secret_manager = StrongholdSecretManager::builder()
-                        .password(std::env::var("WALLET_PASSWORD").unwrap().as_str())
-                        .build(PathBuf::from("wallet.stronghold")).unwrap();
-
                     // Only required the first time, can also be generated with `manager.generate_mnemonic()?`
-                    let mnemonic = generate_mnemonic().unwrap();
-
-                    // The mnemonic only needs to be stored the first time
-                    secret_manager.store_mnemonic(mnemonic).await.unwrap();
-
-                    let manager = AccountManager::builder()
-                        .with_secret_manager(iota_wallet::secret::SecretManager::Stronghold(secret_manager))
+                    let wallet = Wallet::builder()
+                        .with_secret_manager(SecretManager::Stronghold(secret_manager))
                         .with_client_options(client_options)
                         .with_coin_type(SHIMMER_COIN_TYPE)
-                        .finish()
-                        .await.unwrap();
+                        .finish().await.unwrap();
+
+                    // The mnemonic only needs to be stored the first time
+                    let mnemonic = wallet.generate_mnemonic().unwrap();
+                    wallet.store_mnemonic(mnemonic).await.unwrap();
 
                     // Create a new account
-                    manager
+                    wallet
                         .create_account()
                         .with_alias("User".to_string())
                         .finish()
@@ -133,8 +131,8 @@ fn main(){
         .build()
         .expect("Failed creating addresses")
         .block_on(async {
-            let address = account.addresses().await.unwrap()[0].address().to_bech32();
-            println!("Address: {}",&address);
+            let address = account.addresses().await.unwrap()[0].address().to_string();
+            println!("Address: {}", &address);
             return address;
         });
 
@@ -148,23 +146,47 @@ fn main(){
 
     assert_eq!(&bech32_hrp, "edcas");
 
-    println!("Bech32: {}",&bech32_hrp);
+    let mut sig = "0x".to_string();
+    sig.push_str(tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .expect("Failed creating thread")
+        .block_on(async {
+            let secret_manager = StrongholdSecretManager::builder()
+                .password(std::env::var("WALLET_PASSWORD").unwrap().to_string())
+                .build("wallet.stronghold").unwrap();
+            let sig: String =
+                secret_manager.sign_ed25519(&[0, 1],
+                                            Bip44::new(SHIMMER_COIN_TYPE)
+                                                .with_account(0)
+                                                .with_change(false as _)
+                                                .with_address_index(0),
+                ).await.unwrap().public_key().to_bytes().to_hex();
+            return sig;
+        }).as_str());
+
+    println!("Public key: {}", sig);
+    println!("Bech32: {}", &bech32_hrp);
     println!("Done loading wallet!");
     let mut hornet = Hornet {
         node: account.client().clone(),
         account,
+        stronghold: StrongholdSecretManager::builder()
+            .password(std::env::var("WALLET_PASSWORD").unwrap().to_string())
+            .build("wallet.stronghold").unwrap(),
         bus_reader,
     };
-    let eddn = EddnAdapter{
+    let eddn = EddnAdapter {
         hornet_bus,
         queue: VecDeque::new(),
         timestamp: Instant::now(),
     };
-    thread::spawn(move ||{
-        loop{
+    thread::spawn(move || {
+        loop {
             hornet.read();
         }
     });
+    println!("Ready!");
     tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()
